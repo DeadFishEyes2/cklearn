9c0e6292671f1d1a8ca8b273c381eb19d8819615
DeadFishEyes2 <122477630+DeadFishEyes2@users.noreply.github.com>
2025-07-11 01:06:40 +0300

    Tested plots
    I order to visualize the ML algorithms that will be implemented, a good library for data visualization is needed

--- plotc.c
+++ plotc.c
@@ -8,20 +8,29 @@ void plotScatter(float *x, float *y, int size) {
         return;
     }
 
-    fprintf(gp, "set title 'Scatter Plot'\n");
-    fprintf(gp, "plot '-' with points pointtype 7 pointsize 1 lc rgb 'blue'\n");
+    // Set plot style and formatting
+    fprintf(gp, "set title 'Elegant Scatter Plot'\n");
+    fprintf(gp, "set xlabel 'X-axis'\n");
+    fprintf(gp, "set ylabel 'Y-axis'\n");
+    fprintf(gp, "set grid\n");  // Enable grid
+    fprintf(gp, "set key off\n");  // Disable legend if not needed
 
+    // Customize point appearance
+    fprintf(gp, "set style line 1 lc rgb '#0060ad' pt 7 ps 1.5 lw 1\n");  // Blue points
+    fprintf(gp, "plot '-' with points ls 1\n");  // Use the defined style
+
+    // Send data points
     for (int i = 0; i < size; i++) {
         fprintf(gp, "%f %f\n", x[i], y[i]);
     }
-    fprintf(gp, "e\n"); // End of data
+    fprintf(gp, "e\n");  // End of data
 
     fflush(gp);
     pclose(gp);
 }
 
 int main() {
-    float x[] = {1.0, 2.0, 3.0, 4.0, 5.0};
+    float x[] = {1.0, 2.0, 3.0, 4.0, 6.0};
     float y[] = {2.1, 4.2, 6.1, 7.9, 10.2};
     int size = sizeof(x) / sizeof(x[0]);
 
